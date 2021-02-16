c3e3c171c36c940d3e9cd15aca01a41b7ea0daca
piotrslowik <piotr.j.slowik@gmail.com>
2021-02-16 17:17:41 +0100

    Added more statistic methods to Column class

--- src/classes/Column.ts
+++ src/classes/Column.ts
@@ -16,10 +16,25 @@ export default class Column {
   get value(): string {
     return this.nameColumn
       ? 'name'
-      : this.text
-        .trim()
-        .replaceAll(/\.|,|\?|\/|\\|;|;|'|"|\[|\]|\{|\}|<|>|!|@|#|\$|%|\^|&|\*|\(|\)| /g, '_')
-        .toLowerCase();
+      : this.createValue(this.text);
+  }
+  private createValue(name: string): string {
+    const  newName = name
+    .trim()
+    .replaceAll(/\.|,|\?|\/|\\|;|;|'|"|\[|\]|\{|\}|<|>|!|@|#|\$|%|\^|&|\*|\(|\)| /g, '_')
+    .toLowerCase();
+    return this.deletePolishChars(newName);
+  }
+  private deletePolishChars(name: string): string {
+    return name
+      .replaceAll('ą', 'a')
+      .replaceAll('ć', 'c')
+      .replaceAll('ę', 'e')
+      .replaceAll('ł', 'l')
+      .replaceAll('ś', 's')
+      .replaceAll('ó', 'o')
+      .replaceAll('ż', 'z')
+      .replaceAll('ź', 'z')
   }
 
   changeName(name: string) {
@@ -28,5 +43,10 @@ export default class Column {
   average(rows: Row[]): number {
     const sum = rows.reduce((acc, row) => acc += row.values[this.value], 0);
     return sum / rows.length;
+  }
+  changeDestimulantToStimulant(rows: Row[]) {
+    const values = rows.map(row => row.values[this.value]);
+    const max = Math.max(...values);
+    rows.forEach(row => row.values[this.value] = max - row.values[this.value])
   }
 }
